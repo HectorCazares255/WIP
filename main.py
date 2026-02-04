9fc22f7c8f2160b6a35deb0a761b2d917a2976c9
car814435 <car814435@gmail.com>
2026-02-04 15:29:58 -0800

    Added tracking and outputing total shift time in hours and minutes, added if statements to account for single digit minutes

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ from datetime import datetime
 def main():
     choice = input("Would you like to clock in? (yes/no): ")
     if choice.lower() == "yes":
-        current_time = datetime.now()
-        print("Clocked in at:", current_time.hour, ":", current_time.minute)
-        return current_time
+        clock_in_time = datetime.now()
+        shift_start = (clock_in_time.hour * 60) + clock_in_time.minute # Converts shift start time to minutes
+        if clock_in_time.minute <= 9:  # Adds leading zero for single digit minutes
+            clock_in = str(clock_in_time.hour) + ":" + "0" + str(clock_in_time.minute)
+            print("Clocked in at:", clock_in)
+        else:
+            clock_in = str(clock_in_time.hour) + ":" + str(clock_in_time.minute)
+            print("Clocked in at:", clock_in)
+    
+    choice = input("Would you like to clock out? (yes/no): ")
+    if choice.lower() == "yes":
+        clock_out_time = datetime.now()
+        shift_end = (clock_out_time.hour * 60) + clock_out_time.minute  # Converts shift end time to minutes
+        time_worked_hours = (shift_end - shift_start)/60    # Finds total hours worked
+        time_worked_minutes = (shift_end - shift_start)%60  # Finds remaining minutes worked
+        if clock_out_time.minute <= 9:  # Adds leading zero for single digit minutes
+            clock_out = str(clock_out_time.hour) + ":" + "0" + str(clock_out_time.minute)
+            print("Clocked out at:", clock_out)
+        else:
+            clock_out = str(clock_out_time.hour) + ":" + str(clock_out_time.minute)
+            print("Clocked out at:", clock_out)
+        print("Time worked:", int(time_worked_hours), "hour(s) and", time_worked_minutes, "minute(s)")
     return 0
 
 
