3cd80b65564c206f312d70b2dea310c3471e3dc7
car814435 <car814435@gmail.com>
2026-02-04 14:43:53 -0800

    Created main(), prompt user for clock in, and printing clock in time

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from datetime import datetime
 
+def main():
+    choice = input("Would you like to clock in? (yes/no): ")
+    if choice.lower() == "yes":
+        current_time = datetime.now()
+        print("Clocked in at:", current_time.hour, ":", current_time.minute)
+        return current_time
+    return 0
+
+
+if __name__ == "__main__":
+    main()
