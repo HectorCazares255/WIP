23c3f2e66843771def8ecead86f1085d2e8cd878
car814435 <car814435@gmail.com>
2026-02-18 15:06:31 -0800

    Fixed main.py to take shift start an end times from shiftdata.json and output time worked, shiftdata.py now records shift start and end times to shiftdata.json

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import jsonfile
 import shiftdata
 from datetime import datetime
@@ -6,24 +7,28 @@ def clockIn():
     print("Would you like to clock in? (yes/no): ")
     clock_in_response = input()
     if clock_in_response.lower() == "yes":
+        shiftdata.startShift()
         clock_in_time = datetime.now()
-        #shift_start = (clock_in_time.hour * 60) + clock_in_time.minute # Converts shift start time to minutes
         clock_in = clock_in_time.strftime("%H:%M")
         print("Clocked in at:", clock_in)
-        shiftdata.startShift()
 
 def clockOut():
     print("Would you like to clock out? (yes/no): ")
     clock_out_response = input()
     if clock_out_response.lower() == "yes":
+        shiftdata.endShift()
         clock_out_time = datetime.now()
-        #shift_end = (clock_out_time.hour * 60) + clock_out_time.minute  # Converts shift end time to minutes
-        #time_worked_hours = (shift_end - shift_start)/60    # Finds total hours worked
-        #time_worked_minutes = (shift_end - shift_start)%60  # Finds remaining minutes worked
+        with open("shiftdata.json", "r") as file: # Reads the JSON file to get the clock in and clock out time
+            data = json.load(file)
+            clock_in_time = datetime.strptime(data["ClockInTime"], "%H:%M")
+            clock_out_time = datetime.strptime(data["ClockOutTime"], "%H:%M")
+        shift_start = (clock_in_time.hour * 60) + clock_in_time.minute # Converts shift start time to minutes
+        shift_end = (clock_out_time.hour * 60) + clock_out_time.minute  # Converts shift end time to minutes
+        time_worked_hours = (shift_end - shift_start)/60    # Finds total hours worked
+        time_worked_minutes = (shift_end - shift_start)%60  # Finds remaining minutes worked
         clock_out = clock_out_time.strftime("%H:%M")
         print("Clocked out at:", clock_out)
-        #print("Time worked:", int(time_worked_hours), "hour(s) and", time_worked_minutes, "minute(s)")
-        shiftdata.endShift()
+        print("Time worked:", int(time_worked_hours), "hour(s) and", time_worked_minutes, "minute(s)")
 
 def main():
     ##jsonfile.main()
--- shiftdata.py
+++ shiftdata.py
@@ -7,24 +7,26 @@ def startShift():
         with open("shiftdata.json", "r") as file:
             data = json.load(file)
             data["ClockedIn"] = "Yes"
+            data["ClockInTime"] = main.datetime.now().strftime("%H:%M")
             with open("shiftdata.json", "w") as file:
                 json.dump(data, file)
     else:
         print("Error occurred. Trying again...")
         with open("shiftdata.json", "w") as file:
-            json.dump({"ClockedIn": "Yes"}, file)
+            json.dump({"ClockedIn": "Yes", "ClockInTime": main.datetime.now().strftime("%H:%M")}, file)
 
 def endShift():
     if os.path.exists("shiftdata.json"):
         with open("shiftdata.json", "r") as file:
             data = json.load(file)
             data["ClockedIn"] = "No"
+            data["ClockOutTime"] = main.datetime.now().strftime("%H:%M")
             with open("shiftdata.json", "w") as file:
                 json.dump(data, file)
     else:
         print("Error occurred. Trying again...")
         with open("shiftdata.json", "w") as file:
-            json.dump({"ClockedIn": "No"}, file)
+            json.dump({"ClockedIn": "No", "ClockOutTime": main.datetime.now().strftime("%H:%M")}, file)
 
 def checkShift():
     if os.path.exists("shiftdata.json"):
