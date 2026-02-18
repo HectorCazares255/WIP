3f41e3ece7fd5c1644e515c0ed43fd2846f663c7
car814435 <car814435@gmail.com>
2026-02-18 13:23:39 -0800

    Made separate function addEmployee(), added a test case to employeeinfo.json, employee() now compares input to employeeinfo.json and logins in if correct

--- jsonfile.py
+++ jsonfile.py
@@ -7,12 +7,14 @@ def admin():
     email = input("Please enter your email: ")
     password = input("Please enter your password?: ")
 
-#this function is only for employees
-def employee():
-    #asks employee questions about their info
-    name = input("Welcome! Please enter your name: ")
-    email = input("Please enter your email: ")
-    password = input("Please enter your password?: ")
+    add_employee = input("Would you like to add an employee? (yes/no): ")
+    if add_employee.lower() == "yes":
+        addEmployee()
+
+def addEmployee():
+    name = input("Please enter the employee's name: ")
+    email = input("Please enter the employee's email: ")
+    password = input("Please enter the employee's password?: ")
 
     #saves answers in a dictionary
     employee_data = { "Name": name, "Email": email, "Password": password }
@@ -27,6 +29,22 @@ def employee():
         print("Error occurred. Trying again...")
         with open("employeeinfo.json", "w") as file:
             json.dump(employee_data, file)
+
+#this function is only for employees
+def employee():
+    #asks employee questions about their info
+    name = input("Welcome! Please enter your name: ")
+    email = input("Please enter your email: ")
+    password = input("Please enter your password?: ")
+
+    #checking if the JSON file exists, if it does, we compare dictionary info to the JSON file info
+    if os.path.exists("employeeinfo.json"):
+        with open("employeeinfo.json", "r") as file:
+            data = json.load(file)
+            if name == data["Name"] and email == data["Email"] and password == data["Password"]:
+                print("Welcome back", name + "!")
+            else:
+                print("Invalid credentials. Please try again.")
 
 def main():
     #asks if user is admin or employee
